41a176e0fade0e4275dbca72d64ded9b6efcae05
Christi Kerr <christinakerr394@gmail.com>
2020-10-03 15:34:02 -0500

    Current UV index displays. Fixes #4

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var weatherEmojiTodayEl = $("#weatherEmoji");
 var tempMainEl = $("#temp-main");
 var humidityMainEl = $("#humidity-main");
 var windMainEl = $("#wind-main");
+var uvMainEl = $("#UV-main");
 
 var currentCity;
 
@@ -17,19 +18,17 @@ var APIKey = "c9d43ea44c719a1c7bfa973c77a3170b";
 
 function citySearch(event){ // Displays weather data for a new city
     event.preventDefault();
-        currentCity = inputEl.val();
+        currentCity = inputEl.val().trim();
         console.log(currentCity);
-        console.log(cityNameH2);
 
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=" + APIKey;
 
-        if (currentCity != null) {
+        if (currentCity != null) { // Query for basic weather info for city (if city is typed in)
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function (cityWeatherData) {
                 console.log(cityWeatherData)
-                console.log(cityWeatherData.name)
                 cityNameH2.text(cityWeatherData.name);
                 currentDateH2.text("Today");
                 weatherEmojiTodayEl.text("Weather");
@@ -38,6 +37,27 @@ function citySearch(event){ // Displays weather data for a new city
                 humidityMainEl.text("Humidity: " + cityWeatherData.main.humidity);
                 windMainEl.text("Wind Speed: " + cityWeatherData.wind.speed);
 
+                var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityWeatherData.coord.lat + "&lon=" + cityWeatherData.coord.lon + "&appid=" + APIKey;
+                console.log(uvQuery);
+
+                
+                $.ajax({  // Query for UV Index Data
+                    url: uvQuery,
+                    method: "GET"
+                }).then(function(uvData) {
+                    uvMainEl.text("UV Index: " + uvData.value);
+                })
+
+                var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&units=imperial&appid=" + APIKey;
+                
+                $.ajax({
+                    url: fiveDayURL, // Query for 5 Day Forecast
+                    method: "GET"
+                }).then(function(fiveDayData) {
+
+
+
+                })
             })
         }
 }
@@ -45,7 +65,5 @@ function citySearch(event){ // Displays weather data for a new city
 //MAIN PROCESSES
 
 window.addEventListener('load', function () {
-
     searchButton.on("click", citySearch); // When the user clicks search button
-
 })
